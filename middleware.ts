1f1e64e4411462f69a802f7fb0d16c2d3b09b66a
ali-hassnain <ali.hassnain@dubizzlelabs.com>
2025-02-18 00:16:00 +0400

    remove extra api calls

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
-// File: middleware.ts
 import { type NextRequest } from 'next/server'
-import { updateSession } from './supabase/middleware'
+import { updateSession } from '@/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
     return await updateSession(request)
@@ -8,7 +7,13 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
     matcher: [
-        // Apply middleware to all paths except static files, images, favicon, etc.
+        /*
+         * Match all request paths except for the ones starting with:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         * Feel free to modify this pattern to include more paths.
+         */
         '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
