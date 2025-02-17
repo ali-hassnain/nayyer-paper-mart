e05c0d503ccb1e11b9ffac81a490fdf858d8f52a
ali-hassnain <alihassnain5@gmail.com>
2025-02-17 14:41:04 +0400

    middleware revert again

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 // File: middleware.ts
-import { NextRequest } from 'next/server'
+import { type NextRequest } from 'next/server'
 import {updateSession} from "./supabase/middleware";
 
 export async function middleware(request: NextRequest) {
@@ -8,7 +8,13 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
     matcher: [
-        // Match all request paths except for static files, images, or favicon
+        /*
+         * Match all request paths except for the ones starting with:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         * Feel free to modify this pattern to include more paths.
+         */
         '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
